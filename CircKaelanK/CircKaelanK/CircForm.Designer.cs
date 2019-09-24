3405f4caebb56c42bcc3f4c2985dcb4317037c2e
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-09-24 12:02:00 -0400

    circumference

---
+++ CircKaelanK/CircKaelanK/CircForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CircKaelanK
+{
+    partial class frmCirc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.lblCirc = new System.Windows.Forms.Label();
+            this.lblCircAns = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(0, 57);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(317, 25);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Enter the radius of a circle (cm):\r\n";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(454, 48);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(119, 43);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(323, 63);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 3;
+            this.txtRadius.TextChanged += new System.EventHandler(this.TxtRadius_TextChanged);
+            // 
+            // lblCirc
+            // 
+            this.lblCirc.AutoSize = true;
+            this.lblCirc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCirc.Location = new System.Drawing.Point(0, 155);
+            this.lblCirc.Name = "lblCirc";
+            this.lblCirc.Size = new System.Drawing.Size(341, 25);
+            this.lblCirc.TabIndex = 4;
+            this.lblCirc.Text = "The circumference of the circle is: ";
+            // 
+            // lblCircAns
+            // 
+            this.lblCircAns.AutoSize = true;
+            this.lblCircAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircAns.Location = new System.Drawing.Point(375, 155);
+            this.lblCircAns.Name = "lblCircAns";
+            this.lblCircAns.Size = new System.Drawing.Size(48, 25);
+            this.lblCircAns.TabIndex = 6;
+            this.lblCircAns.Text = "???";
+            // 
+            // frmCirc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCircAns);
+            this.Controls.Add(this.lblCirc);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmCirc";
+            this.Text = "Circumference of a Circle, Kaelan K";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblCirc;
+        private System.Windows.Forms.Label lblCircAns;
+    }
+}
+
---
+++ CircKaelanK/CircKaelanK/CircForm.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by: Kaelan K
+ * Created on: 19-09-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #11 - Circumference of a circle
+ * This program takes the users radius and calculates the circumference
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircKaelanK
+{
+    public partial class frmCirc : Form
+    {
+        public frmCirc()
+        {
+            InitializeComponent();
+
+            // hiding labels
+            this.lblCirc.Hide();
+            this.lblCircAns.Hide();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TxtRadius_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // calculate the circumference with radius
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // defining var
+            double radius, circ;
+
+            // converting txtbox to double
+            radius = double.Parse(txtRadius.Text);
+
+            // calculating the circumference
+            circ = 3.14 * Math.Pow(radius, 2);
+
+            // converting double back to text
+            this.lblCircAns.Text = Convert.ToString(circ + " cm");
+
+            // showing labels again
+            this.lblCirc.Show();
+            this.lblCircAns.Show();
+
+        }
+    }
+}
